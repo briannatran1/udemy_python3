9b62d07bc01aa2684ecd4f48eac32abb1d613415
maserabri <briannatran45@gmail.com>
2023-05-15 12:15:22 -0700

    adding bouncer

---
+++ conditional_logic/bouncer.py
@@ -0,0 +1,19 @@
+# ask for age
+
+# 18 - 20 wristband
+# 21+ drink, normal entry
+# too young, sorry
+
+# nested statements
+
+age = input('How old are you: ')
+if age: # if age isn't empty
+    age = int(age)
+    if age >= 18 and age <21:
+        print('You can enter, but need a wristband!')
+    elif age >= 21:
+        print('You are good to enter and can drink!')
+    else:
+        print("You can't come in little one! :(")
+else:
+    print('Please enter an age!')
