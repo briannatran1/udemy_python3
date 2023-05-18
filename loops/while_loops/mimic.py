b67fba9de12550003d4caaf729732b127fadc10b
maserabri <briannatran45@gmail.com>
2023-05-18 14:43:56 -0700

    added mimic

---
+++ loops/while_loops/mimic.py
@@ -0,0 +1,7 @@
+#repeat everything until the user says 'stop copying me'
+
+msg = input("Say something: ")
+while msg != 'stop copying me':
+    print(msg)
+    msg = input()
+print('UGH FINE YOU WIN')
