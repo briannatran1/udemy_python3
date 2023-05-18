2824e0bc326d978be3884be71aba98bc50354ff5
maserabri <briannatran45@gmail.com>
2023-05-18 14:34:50 -0700

    added nested loop

--- loops/while_loops/emoji_art.py
+++ loops/while_loops/emoji_art.py
@@ -11,3 +11,9 @@ num = 1
 while num <= 10:
     print("\U0001f600" * num) 
     num += 1
+
+# nested loop:
+
+for x in range(3): # repeats inner loop 3 times
+    for num in range(1, 11):
+        print("\U0001f600" * num) 
