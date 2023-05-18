85399febb475885011ccec033b75b2958dbe1e91
maserabri <briannatran45@gmail.com>
2023-05-18 14:32:50 -0700

    added while loops

---
+++ loops/while_loops/emoji_art.py
@@ -0,0 +1,13 @@
+# print emoji using both a for loop and a while loop
+
+# for loop:
+
+for num in range(1, 11):
+    print("\U0001f600" * num) 
+
+# while loop:
+
+num = 1
+while num <= 10:
+    print("\U0001f600" * num) 
+    num += 1
