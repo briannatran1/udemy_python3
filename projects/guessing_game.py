657851f4f61411ea434c3cd59db1dcebed7d1c2f
maserabri <briannatran45@gmail.com>
2023-05-22 22:13:11 -0700

    updated guessing game

--- projects/guessing_game.py
+++ projects/guessing_game.py
@@ -1,14 +1,14 @@
 import random
 
 random_number = random.randint(1,10) # numbers 1 = 10
-guess = None
+
 #handle user guesses
   #if they guess correct, tell them they won
   #otherwise, tell them if they are too high or too low
 
 #BONUS - let player play again if they want
 
-while guess != random_number:
+while True:
   guess = int(input('Guess a number between 1 and 10: '))
   if guess < random_number:
     print('Too low, try again!')
