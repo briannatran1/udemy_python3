81679f2d711194df197c599c0d8b1ed009aee859
maserabri <briannatran45@gmail.com>
2023-06-07 20:51:15 -0700

    added accessing values ex

---
+++ lists/accessing_list_data.py
@@ -0,0 +1,14 @@
+# I'm having a party, and made a list of people I want to invite.  Unfortunately, I'm a terrible friend and made a couple of 
+# spelling errors.  Help me correct them!
+
+# Change "Hanna" to "Hannah" (there should be an 'h' at the end)
+
+# Change "Geoffrey" to "Jeffrey"
+
+# Change "aparna" to "Aparna" (capitalize it)
+
+people = ["Hanna","Louisa","Claudia", "Angela","Geoffrey", "aparna"]
+
+people[0] = 'Hannah'
+people[4] = 'Jeffrey'
+people[-1] = 'Aparna'
